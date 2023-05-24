973671a941af6194bbf94515ba085ecadc07ebf4
Ayan <ajan.kohan@gmail.com>
2023-05-25 02:22:26 +0600

    M3 task3

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -4,7 +4,7 @@ public class DeclaringVars {
     public static void main(String[] args) {
         //write code here
         int a,b,c;
-        a = b =  c = 10;
+        a = b = c = 10;
         System.out.println(c);
     }
 }
